b9a637a9364a52323f088d2d6fc03984fca0ab7a
장동겸 <jangelliot0404@dgu.ac.kr>
2025-02-28 14:15:50 +0900

    🔨 Fix - 이메일 폼 CSS 인라인 형식으로 변경 (#172)

--- src/main/java/com/tookscan/tookscan/core/utility/MailUtil.java
+++ src/main/java/com/tookscan/tookscan/core/utility/MailUtil.java
@@ -11,96 +11,42 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MailUtil {
     private static final String TEST_EMAIL_TEMPLATE = """
-            <!doctype html>
-            <html lang="ko">
-              <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>테스트 이메일</title>
-                <style>
-                  body {
-                    margin: 0;
-                    padding: 64px;
-                    font-family: Arial, sans-serif;
-                    background-color: #ffffff;
-                  }
-                  .container {
-                    width: 100%;
-                    max-width: 600px;
-                    margin: 0 auto;
-                    background: #f2f7ff;
-                    padding: 3.125rem;
-                    box-sizing: border-box;
-                    border-radius: 1.5rem;
-                  }
-                  .blue-bar {
-                    width: 100%;
-                    height: 3px;
-                    background-color: #5592fc;
-                  }
-            
-                  .title-section {
-                    text-align: center;
-                    padding: 3.25rem;
-                    margin-top: 5rem;
-                  }
-                  .title-section h1 {
-                    font-size: 2rem;
-                    color: #5592fc;
-                    margin: 0;
-                    text-align: center;
-                  }
-                  .title-section p {
-                    font-size: 1.25rem;
-                    color: #777777;
-                    text-align: center;
-                    margin-top: 8px;
-                  }
-                  .img {
-                    text-align: center;
-                  }
-                  .img img {
-                    max-width: 250px;
-                    height: auto;
-                  }
-                  .message-section {
-                    padding: 20px;
-                    font-size: 1.25rem;
-                    color: #333;
-                    text-align: start;
-                    margin-top: 3.125rem;
-                    margin-bottom: 5rem;
-                  }
-                </style>
-              </head>
-              <body>
-                <div class="container">
-                  <div class="blue-bar"></div>
-            
-                  <div class="title-section">
-                    <h1>테스트 이메일이 도착했어요!</h1>
-                    <p>가장 합리적인 비대면 셀프 스캔, 한방에 툭스캔!</p>
-                  </div>
-            
-                  <div class="img">
-                    <img src="https://i.ibb.co/HTS5jm9m/testmail.png" alt="testmail" />
-                  </div>
-            
-                  <div class="message-section">
-                    <p>
-                      안녕하세요, 요청하신 테스트 이메일을 발송드렸습니다.<br /><br />
-                      이메일 주소를 확인하셨다면,<br />
-                      마지막 단계까지 신청서를 작성하여 서비스 신청을 완료해보세요!<br /><br />
-                      툭스캔과 함께 더 편리한 서비스를 경험하실 수 있도록<br />
-                      항상 노력하겠습니다 :)
-                    </p>
-                  </div>
-            
-                  <div class="blue-bar"></div>
-                </div>
-              </body>
-            </html>
-            """;
+<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>테스트 이메일</title>
+  </head>
+  <body style="margin: 0; padding: 64px; font-family: Arial, sans-serif; background-color: #ffffff;">
+    <div class="container" style="width: 100%; max-width: 600px; margin: 0 auto; background: #f2f7ff; padding: 3.125rem; box-sizing: border-box; border-radius: 1.5rem;">
+      <div class="blue-bar" style="width: 100%; height: 3px; background-color: #5592fc;"></div>
+      
+      <div class="title-section" style="text-align: center; padding: 3.25rem; margin-top: 5rem;">
+        <h1 style="font-size: 2rem; color: #5592fc; margin: 0; text-align: center;">테스트 이메일이 도착했어요!</h1>
+        <p style="font-size: 1.25rem; color: #777777; text-align: center; margin-top: 8px;">가장 합리적인 비대면 셀프 스캔, 한방에 툭스캔!</p>
+      </div>
+      
+      <div class="img" style="text-align: center;">
+        <img src="https://i.ibb.co/HTS5jm9m/testmail.png" alt="testmail" style="max-width: 250px; height: auto;" />
+      </div>
+      
+      <div class="message-section" style="padding: 20px; font-size: 1.25rem; color: #333; text-align: start; margin-top: 3.125rem; margin-bottom: 5rem;">
+        <p>
+          안녕하세요, 요청하신 테스트 이메일을 발송드렸습니다.<br /><br />
+          이메일 주소를 확인하셨다면,<br />
+          마지막 단계까지 신청서를 작성하여 서비스 신청을 완료해보세요!<br /><br />
+          툭스캔과 함께 더 편리한 서비스를 경험하실 수 있도록<br />
+          항상 노력하겠습니다 :)
+        </p>
+      </div>
+      
+      <div class="blue-bar" style="width: 100%; height: 3px; background-color: #5592fc;"></div>
+    </div>
+  </body>
+</html>
+""";
+
 
     private static final String FORGET_PASSWORD_TEMPLATE = """
             <div style="width: 540px; border-top: 4px solid #02b875; border-bottom: 4px solid #02b875; margin: 100px auto; padding: 30px 0; box-sizing: border-box;">
