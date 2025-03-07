83d55a2ad4022927cb4d7045539e17ef7132e1eb
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-07 22:58:46 +0900

    fix/#209 : 약관 수정 API에 약관 삭제 로직 포함되도록 변경

--- src/main/java/com/tookscan/tookscan/term/application/controller/command/TermAdminCommandV1Controller.java
+++ src/main/java/com/tookscan/tookscan/term/application/controller/command/TermAdminCommandV1Controller.java
@@ -4,7 +4,6 @@ import com.tookscan.tookscan.core.dto.ResponseDto;
 import com.tookscan.tookscan.term.application.dto.request.CreateAdminTermRequestDto;
 import com.tookscan.tookscan.term.application.dto.request.UpdateAdminTermRequestDto;
 import com.tookscan.tookscan.term.application.usecase.CreateAdminTermUseCase;
-import com.tookscan.tookscan.term.application.usecase.DeleteAdminTermUseCase;
 import com.tookscan.tookscan.term.application.usecase.UpdateAdminTermUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +16,6 @@ public class TermAdminCommandV1Controller {
 
     private final CreateAdminTermUseCase createAdminTermUseCase;
     private final UpdateAdminTermUseCase updateAdminTermUseCase;
-    private final DeleteAdminTermUseCase deleteAdminTermUseCase;
 
     /**
      * 8.1.1 (관리자) 약관 추가
@@ -38,17 +36,6 @@ public class TermAdminCommandV1Controller {
             @RequestBody @Valid UpdateAdminTermRequestDto requestDto
     ) {
         updateAdminTermUseCase.execute(requestDto);
-        return ResponseDto.ok(null);
-    }
-
-    /**
-     * 8.5.1 (관리자) 약관 삭제
-     */
-    @DeleteMapping("/terms/{id}")
-    public ResponseDto<Void> deleteAdminTerm(
-            @PathVariable Long id
-    ) {
-        deleteAdminTermUseCase.execute(id);
         return ResponseDto.ok(null);
     }
 }
--- src/main/java/com/tookscan/tookscan/term/application/service/UpdateAdminTermService.java
+++ src/main/java/com/tookscan/tookscan/term/application/service/UpdateAdminTermService.java
@@ -12,94 +12,67 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateAdminTermService implements UpdateAdminTermUseCase {
 
     private final TermRepository termRepository;
-
     private final TermService termService;
 
     @Override
     @Transactional
     public void execute(UpdateAdminTermRequestDto requestDto) {
 
-        boolean isSignUpChanged = requestDto.terms().stream()
-                .anyMatch(dto -> dto.type() == ETermType.SIGN_UP);
+        List<Term> terms = termRepository.findAllByTypeOrElseThrow(
+                requestDto.terms().get(0).type()
+        );
 
-        boolean isScanChanged = requestDto.terms().stream()
-                .anyMatch(dto -> dto.type() == ETermType.SCAN);
-
-        List<Term> terms = termRepository.findAll();
-
-        List<Term> signupTerms = terms.stream()
-                .filter(term -> term.getType() == ETermType.SIGN_UP)
-                .toList();
-        List<Term> scanTerms = terms.stream()
-                .filter(term -> term.getType() == ETermType.SCAN)
-                .toList();
-        int signupSize = signupTerms.size();
-        int scanSize = scanTerms.size();
-
-        Map<Integer, Boolean> checkSignupSortOrder = new HashMap<>();
-        Map<Integer, Boolean> checkScanSortOrder = new HashMap<>();
-
-        for (int i = 1; i <= signupSize; i++) {
-            checkSignupSortOrder.put(i, false);
-        }
-
-        for (int i = 1; i <= scanSize; i++) {
-            checkScanSortOrder.put(i, false);
-        }
-
-        requestDto.terms().forEach(dto -> {
-            if (dto.type() == ETermType.SIGN_UP) {
-                if (dto.sortOrder() < 0 || dto.sortOrder() > signupSize) {
-                    throw new CommonException(ErrorCode.SORT_ORDER_OUT_OF_RANGE);
-                }
-                if (checkSignupSortOrder.get(dto.sortOrder())) {
-                    throw new CommonException(ErrorCode.SORT_ORDER_DUPLICATE);
-                }
-                checkSignupSortOrder.put(dto.sortOrder(), true);
+        for (Term term : terms) {
+            if (requestDto.terms().stream().noneMatch(dto -> dto.id().equals(term.getId()))) {
+                termRepository.deleteById(term.getId());
             }
-
-            if (dto.type() == ETermType.SCAN) {
-                if (dto.sortOrder() < 0 || dto.sortOrder() > scanSize) {
-                    throw new CommonException(ErrorCode.SORT_ORDER_OUT_OF_RANGE);
-                }
-                if (checkScanSortOrder.get(dto.sortOrder())) {
-                    throw new CommonException(ErrorCode.SORT_ORDER_DUPLICATE);
-                }
-                checkScanSortOrder.put(dto.sortOrder(), true);
-            }
-        });
-
-        if (isSignUpChanged && checkSignupSortOrder.containsValue(false)) {
-            throw new CommonException(ErrorCode.SORT_ORDER_NOT_CONTINUOUS);
         }
 
-        if (isScanChanged && checkScanSortOrder.containsValue(false)) {
-            throw new CommonException(ErrorCode.SORT_ORDER_NOT_CONTINUOUS);
-        }
+        processUpdateTerms(requestDto);
+    }
+
+    private void processUpdateTerms(UpdateAdminTermRequestDto requestDto) {
+        List<Term> terms = termRepository.findAllByTypeOrElseThrow(ETermType.SIGN_UP);
+        validateSortOrder(requestDto, terms.size());
 
         requestDto.terms().forEach(dto -> {
             Term term = terms.stream()
                     .filter(t -> t.getId().equals(dto.id()))
                     .findFirst()
                     .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_TERM));
+
             termService.updateTerm(
-                    term,
-                    dto.title(),
-                    dto.content(),
-                    dto.isRequired(),
-                    dto.isVisible(),
-                    dto.sortOrder()
+                    term, dto.title(), dto.content(),
+                    dto.isRequired(), dto.isVisible(), dto.sortOrder()
             );
             termRepository.save(term);
         });
+    }
+
+    private void validateSortOrder(UpdateAdminTermRequestDto requestDto, int size) {
+        Set<Integer> uniqueOrders = new HashSet<>();
+
+        requestDto.terms().forEach(dto -> {
+            int sortOrder = dto.sortOrder();
+            if (sortOrder < 1 || sortOrder > size) {
+                throw new CommonException(ErrorCode.SORT_ORDER_OUT_OF_RANGE);
+            }
+            if (!uniqueOrders.add(sortOrder)) {
+                throw new CommonException(ErrorCode.SORT_ORDER_DUPLICATE);
+            }
+        });
+
+        if (uniqueOrders.size() != size) {
+            throw new CommonException(ErrorCode.SORT_ORDER_NOT_CONTINUOUS);
+        }
     }
 }
