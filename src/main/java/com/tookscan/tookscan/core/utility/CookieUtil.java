3482a7f7641a5b4ea01708f22cd4ce127e90fb7e
장동겸 <jangelliot0404@dgu.ac.kr>
2025-03-11 16:10:25 +0900

    🔨 Fix - 쿠키 Domain에 누락된 . 추가 (#260)
    fix/#259 : 쿠키 Domain에 누락된 . 추가

--- src/main/java/com/tookscan/tookscan/core/utility/CookieUtil.java
+++ src/main/java/com/tookscan/tookscan/core/utility/CookieUtil.java
@@ -5,7 +5,6 @@ import com.tookscan.tookscan.core.exception.type.CommonException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -13,14 +12,13 @@ import java.util.Optional;
 /**
  * Cookie 관련 유틸리티 클래스
  */
-@Slf4j
 public class CookieUtil {
 
     /**
      * Request에 있는 Cookie 중 name에 해당하는 값을 찾아 반환한다.
      *
      * @param request HttpServletRequest
-     * @param name 찾을 Cookie 이름
+     * @param name    찾을 Cookie 이름
      * @return Optional<String>
      */
     public static Optional<String> refineCookie(HttpServletRequest request, String name) {
@@ -38,10 +36,10 @@ public class CookieUtil {
     /**
      * Response에 Cookie를 추가한다.
      *
-     * @param response HttpServletResponse
+     * @param response     HttpServletResponse
      * @param cookieDomain Cookie 도메인
-     * @param name Cookie 이름
-     * @param value Cookie 값
+     * @param name         Cookie 이름
+     * @param value        Cookie 값
      */
     public static void addCookie(HttpServletResponse response, String cookieDomain, String name, String value) {
         Cookie cookie = new Cookie(name, value);
@@ -53,11 +51,11 @@ public class CookieUtil {
     /**
      * Response에 Secure Cookie를 추가한다.
      *
-     * @param response HttpServletResponse
+     * @param response     HttpServletResponse
      * @param cookieDomain Cookie 도메인
-     * @param name Cookie 이름
-     * @param value Cookie 값
-     * @param maxAge Cookie 만료 시간
+     * @param name         Cookie 이름
+     * @param value        Cookie 값
+     * @param maxAge       Cookie 만료 시간
      */
     public static void addSecureCookie(HttpServletResponse response, String cookieDomain, String name, String value, Integer maxAge) {
         Cookie cookie = new Cookie(name, value);
@@ -72,9 +70,9 @@ public class CookieUtil {
     /**
      * Request에 있는 Cookie 중 name에 해당하는 값을 삭제한다.
      *
-     * @param request HttpServletRequest
+     * @param request  HttpServletRequest
      * @param response HttpServletResponse
-     * @param name 삭제할 Cookie 이름
+     * @param name     삭제할 Cookie 이름
      */
     public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String cookieDomain, String name) {
         Cookie[] cookies = request.getCookies();
@@ -86,14 +84,11 @@ public class CookieUtil {
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals(name)) {
                 Cookie removedCookie = new Cookie(name, null);
-                removedCookie.setDomain(cookieDomain);
+                removedCookie.setDomain("." + cookieDomain);
                 removedCookie.setPath("/");
                 removedCookie.setMaxAge(0);
-
-                if (name.equals("refresh_token")) {
-                    removedCookie.setSecure(true);
-                    removedCookie.setHttpOnly(true);
-                }
+                removedCookie.setSecure(true);
+                removedCookie.setHttpOnly(true);
 
                 response.addCookie(removedCookie);
             }
