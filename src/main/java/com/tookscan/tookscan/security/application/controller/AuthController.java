c9c027dca9fe729d8e83934892d8b3ebd62c37a7
장동겸 <jangelliot0404@dgu.ac.kr>
2025-03-11 15:21:46 +0900

    🌏 Deploy - v1.1.2 (#254)
    * 📃 Docs - prod cicd 설정 추가 (#216)
    
    * 📃 Docs - prod 환경 브랜치 명 "main"으로 변경 (#221)
    
    * 🔨Fix - 도커 허브 로그인 과정 누락으로 인한 버그 수정 (#224)
    
    * ✨ Feature - 쿠폰 발급 기능 API 구현 (#227)
    
    * fix/#220: 잘못된 주문금액 계산방식 수정
    
    * feat/#220: 관리자 쿠폰 생성 API 구현
    
    * 🔨 Fix - 쿠폰 정보 조회 기능 수정 (#229)
    
    fix/#228: 쿠폰 정보 조회 수정
    
    * 🔨 Fix - Auth Briefs 호출 시, 잘못된 쿠키일 경우 쿠키 삭제 로직 추가 (#235)
    
    fix/#234 : Auth Briefs 호출 시, 잘못된 쿠키일 경우 쿠키 삭제 로직 추가
    
    * ✨ Feature - 카카오톡 메세지 전송 기능 구현 (#233)
    
    * chore/#157 : solapi 의존성 추가
    
    * feat/#157 : 3.5 관리자 pdf 전송 API 구현
    
    * feat/#157 : 3.6 관리자 결제 요청 API 구현
    
    * feat/#157 : 3.5 관리자 pdf 전송 API 구현
    
    * feat/#157 : 각 케이스 별 카카오 알림톡 전송 기능 구현
    
    * feat/#157 : content 길이 3000으로 조정
    
    * chore/#157 : 충돌 해결
    
    * fix/#157 : solapi 환경변수로 sender 지정
    
    * fix/#157 : 도메인 메소드 적용
    
    * fix/#157 : sender를 util로 이동
    
    * 🔨 Fix - sender 주입 오타 수정 (#239)
    
    fix/#238 : sender 주입 오타 수정
    
    * 🔨 Fix - 배송 리스트 조회 API 명세에 맞게 수정 (#241)
    
    fix/#236: 관리자 주문 조회 API 명세에 맞게 수정
    
    * ✨ Feature - 회원 전용 쿠폰 기능 추가 (#244)
    
    Feature/#243: 회원 전용 쿠폰 기능 추가
    
    * 🔨 Fix - 잘못된 쿠키로 요청 시, 쿠키 삭제에 대한 책임을 JwtFilter로 이관 (#245)
    
    fix/#242 : 잘못된 쿠키로 요청 시, 쿠키 삭제에 대한 책임을 JwtFilter로 이관
    
    * 🔨 Fix - 결제 요청 시 Proxy 오류 수정 (#247)
    
    fix/#246 : 누락된 Transactional 추가
    
    * 🔨 Fix - 쿠키 삭제되지 않는 버그 수정 (#252)
    
    fix/#251 : 쿠키 삭제되지 않는 버그 수정
    
    ---------
    
    Co-authored-by: Gihwan Kang <100124081+GiHwan2@users.noreply.github.com>

--- src/main/java/com/tookscan/tookscan/security/application/controller/AuthController.java
+++ src/main/java/com/tookscan/tookscan/security/application/controller/AuthController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
