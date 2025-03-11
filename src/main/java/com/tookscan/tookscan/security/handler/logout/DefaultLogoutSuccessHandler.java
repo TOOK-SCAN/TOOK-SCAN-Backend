f3f5bf7edbb8f4c73c02f2c8b5dd3b73702c5901
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-11 16:13:33 +0900

    fix/#259 : 빌드 에러 수정

--- src/main/java/com/tookscan/tookscan/security/handler/logout/DefaultLogoutSuccessHandler.java
+++ src/main/java/com/tookscan/tookscan/security/handler/logout/DefaultLogoutSuccessHandler.java
@@ -8,6 +8,7 @@ import com.tookscan.tookscan.security.handler.common.AbstractFailureHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -21,6 +22,9 @@ public class DefaultLogoutSuccessHandler
         extends AbstractFailureHandler implements LogoutSuccessHandler {
 
     private final HttpServletUtil httpServletUtil;
+
+    @Value("${web-engine.cookie-domain}")
+    private String cookieDomain;
 
     @Override
     public void onLogoutSuccess(
@@ -38,9 +42,9 @@ public class DefaultLogoutSuccessHandler
 
         // 브라우저에서 온 요청인 경우 쿠키를 삭제함
         if (userAgent != null && userAgent.contains("Mozilla")) {
-            CookieUtil.deleteCookie(request, response, Constants.ACCESS_TOKEN);
-            CookieUtil.deleteCookie(request, response, Constants.REFRESH_TOKEN);
-            CookieUtil.deleteCookie(request, response, "JSESSIONID");
+            CookieUtil.deleteCookie(request, response, cookieDomain, Constants.ACCESS_TOKEN);
+            CookieUtil.deleteCookie(request, response, cookieDomain, Constants.REFRESH_TOKEN);
+            CookieUtil.deleteCookie(request, response, cookieDomain, "JSESSIONID");
         }
 
         httpServletUtil.onSuccessBodyResponse(response, HttpStatus.OK);
