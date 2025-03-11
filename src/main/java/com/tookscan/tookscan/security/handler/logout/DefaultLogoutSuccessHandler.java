73f4ca8bb5d1f20472662a026d47e462927a838d
장동겸 <jangelliot0404@dgu.ac.kr>
2025-03-11 17:24:28 +0900

    ♻️ Refactor - 불필요 코드 제거 (#274)
    refactor/#273 : 불필요 코드 제거

--- src/main/java/com/tookscan/tookscan/security/handler/logout/DefaultLogoutSuccessHandler.java
+++ src/main/java/com/tookscan/tookscan/security/handler/logout/DefaultLogoutSuccessHandler.java
@@ -8,7 +8,6 @@ import com.tookscan.tookscan.security.handler.common.AbstractFailureHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -22,9 +21,6 @@ public class DefaultLogoutSuccessHandler
         extends AbstractFailureHandler implements LogoutSuccessHandler {
 
     private final HttpServletUtil httpServletUtil;
-
-    @Value("${web-engine.cookie-domain}")
-    private String cookieDomain;
 
     @Override
     public void onLogoutSuccess(
