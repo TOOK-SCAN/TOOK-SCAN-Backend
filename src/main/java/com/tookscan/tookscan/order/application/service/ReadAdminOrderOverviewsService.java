9787399244ce54e9bf7b9c5e77bb800c0a18c0bc
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-13 15:03:09 +0900

    fix/#318 : 불필요 로그 제거

--- src/main/java/com/tookscan/tookscan/order/application/service/ReadAdminOrderOverviewsService.java
+++ src/main/java/com/tookscan/tookscan/order/application/service/ReadAdminOrderOverviewsService.java
@@ -5,22 +5,16 @@ import com.tookscan.tookscan.order.application.usecase.ReadAdminOrderOverviewsUs
 import com.tookscan.tookscan.order.domain.Order;
 import com.tookscan.tookscan.order.domain.type.EOrderStatus;
 import com.tookscan.tookscan.order.repository.OrderRepository;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ReadAdminOrderOverviewsService implements ReadAdminOrderOverviewsUseCase {
--- src/main/java/com/tookscan/tookscan/order/repository/mysql/OrderRepositoryImpl.java
+++ src/main/java/com/tookscan/tookscan/order/repository/mysql/OrderRepositoryImpl.java
@@ -10,23 +10,18 @@ import com.tookscan.tookscan.order.domain.Order;
 import com.tookscan.tookscan.order.domain.QOrder;
 import com.tookscan.tookscan.order.domain.type.EOrderStatus;
 import com.tookscan.tookscan.order.repository.OrderRepository;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Repository;
 
-@Slf4j
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepository {
