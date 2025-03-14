92b11f9ff8a1a44ca7b3c3baa809e9fab8d98585
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-15 02:26:24 +0900

    fix/#332 : 주석 수정

--- src/main/java/com/tookscan/tookscan/security/application/controller/AuthController.java
+++ src/main/java/com/tookscan/tookscan/security/application/controller/AuthController.java
@@ -126,7 +126,7 @@ public class AuthController {
      */
     @Deprecated
     @GetMapping("/briefs")
-    @Operation(summary = "계정 간단 정보 조회 (더 이상 사용되지 않음)", description = "계정 유형(ADMIN, USER)과 이름을 포함한 유저의 기본 정보를 조회합니다.")
+    @Operation(summary = "계정 간단 정보 조회", description = "계정 유형(ADMIN, USER)과 이름을 포함한 유저의 기본 정보를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "조회 성공"),
             @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
