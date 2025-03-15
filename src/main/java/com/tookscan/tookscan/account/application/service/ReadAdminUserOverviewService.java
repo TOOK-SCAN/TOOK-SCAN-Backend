a38c81d3f7af74fd6b954551ed614b4478cd1fda
Gihwan Kang <100124081+GiHwan2@users.noreply.github.com>
2025-03-15 18:20:46 +0900

    ♻️ Refactor - 3.2.4 (관리자) 유저 리스트 조회 날짜 파라미터 형식 수정 (#339)
    refactor/#338: 3.2.4 (관리자) 유저 리스트 조회 날짜 파라미터 형식 수정

--- src/main/java/com/tookscan/tookscan/account/application/service/ReadAdminUserOverviewService.java
+++ src/main/java/com/tookscan/tookscan/account/application/service/ReadAdminUserOverviewService.java
@@ -8,15 +8,14 @@ import com.tookscan.tookscan.core.dto.PageInfoDto;
 import com.tookscan.tookscan.core.utility.DateTimeUtil;
 import com.tookscan.tookscan.order.domain.Order;
 import com.tookscan.tookscan.order.repository.OrderRepository;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -44,8 +43,8 @@ public class ReadAdminUserOverviewService implements ReadAdminUserOverviewUseCas
                 searchType,
                 search,
                 groupId,
-                startDate != null ? DateTimeUtil.convertStringToDartDate(startDate) : null,
-                endDate != null ? DateTimeUtil.convertStringToDartDate(endDate) : null,
+                startDate != null ? DateTimeUtil.convertStringToLocalDate(startDate) : null,
+                endDate != null ? DateTimeUtil.convertStringToLocalDate(endDate) : null,
                 pageable
         );
 
