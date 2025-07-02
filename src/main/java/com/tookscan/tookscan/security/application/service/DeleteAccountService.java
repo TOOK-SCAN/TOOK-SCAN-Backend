6f8473c4282e2dfee7f9213dd357df871725e253
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-07-03 01:26:36 +0900

    feat/#525: 회월 탈퇴 시 탈퇴 사유 입력 추가

--- src/main/java/com/tookscan/tookscan/security/application/service/DeleteAccountService.java
+++ src/main/java/com/tookscan/tookscan/security/application/service/DeleteAccountService.java
@@ -1,6 +1,8 @@
 package com.tookscan.tookscan.security.application.service;
 
 import com.tookscan.tookscan.security.application.usecase.DeleteAccountUseCase;
+import com.tookscan.tookscan.security.domain.mysql.Account;
+import com.tookscan.tookscan.security.presentation.dto.request.DeleteAccountRequestDto;
 import com.tookscan.tookscan.security.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,14 @@ public class DeleteAccountService implements DeleteAccountUseCase {
     private final AccountRepository accountRepository;
 
     @Override
-    public void execute(UUID accountId) {
+    public void execute(UUID accountId, DeleteAccountRequestDto requestDto) {
+        // 계정 조회
+        Account account = accountRepository.findByIdOrElseThrow(accountId);
+
+        // 계정 탈퇴 사유 업데이트
+        account.updateReasonDeletion(requestDto.reason());
+        accountRepository.save(account);
+
         accountRepository.deleteById(accountId);
     }
 }
