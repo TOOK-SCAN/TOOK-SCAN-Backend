e8d4cc5edc25313fbbf489f167a9c90cc1ed2fba
Gihwan Kang <100124081+GiHwan2@users.noreply.github.com>
2025-07-22 00:42:25 +0900

    🔨 Fix - 중복 책 검증 로직 삭제 (#574)
    fix/#573: 중복 책 이름 검사 로직 삭제

--- src/main/java/com/tookscan/tookscan/order/domain/service/DocumentService.java
+++ src/main/java/com/tookscan/tookscan/order/domain/service/DocumentService.java
@@ -1,7 +1,5 @@
 package com.tookscan.tookscan.order.domain.service;
 
-import com.tookscan.tookscan.core.exception.error.ErrorCode;
-import com.tookscan.tookscan.core.exception.type.CommonException;
 import com.tookscan.tookscan.order.domain.Document;
 import com.tookscan.tookscan.order.domain.Order;
 import com.tookscan.tookscan.order.domain.type.ERecoveryOption;
@@ -19,10 +17,6 @@ public class DocumentService {
             Integer additionalPriceForOcr,
             Boolean isOcrEnabled
     ) {
-        // 중복된 이름 검사
-        if (order.getDocuments().stream().anyMatch(doc -> doc.getName().equals(name))) {
-            throw new CommonException(ErrorCode.DUPLICATE_DOCUMENT_NAME, "문서 이름: " + name);
-        }
 
         Document document = Document.builder()
                 .name(name)
@@ -48,10 +42,6 @@ public class DocumentService {
             boolean isOcrEnabled,
             Integer additionalPriceForOcr
     ) {
-        // 중복된 이름 검사
-        if (document.getOrder().getDocuments().stream().anyMatch(doc -> doc.getName().equals(name))) {
-            throw new CommonException(ErrorCode.DUPLICATE_DOCUMENT_NAME, "문서 이름: " + name);
-        }
 
         document.updateName(name);
         document.updatePageCount(pageCount);
