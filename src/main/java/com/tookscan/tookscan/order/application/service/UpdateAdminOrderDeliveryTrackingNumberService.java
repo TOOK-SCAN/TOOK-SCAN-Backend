6c523f378dfe04618408fb182ba92f6c48829ef7
GiHwan2 <sandwich5011@naver.com>
2025-07-24 01:22:58 +0900

    Fix/#576: 배송대기 상태 검증

--- src/main/java/com/tookscan/tookscan/order/application/service/UpdateAdminOrderDeliveryTrackingNumberService.java
+++ src/main/java/com/tookscan/tookscan/order/application/service/UpdateAdminOrderDeliveryTrackingNumberService.java
@@ -1,10 +1,12 @@
 package com.tookscan.tookscan.order.application.service;
 
+import com.tookscan.tookscan.core.exception.error.ErrorCode;
 import com.tookscan.tookscan.core.utility.KakaoMessageUtil;
 import com.tookscan.tookscan.order.application.usecase.UpdateAdminOrderDeliveryTrackingNumberUseCase;
 import com.tookscan.tookscan.order.domain.Delivery;
 import com.tookscan.tookscan.order.domain.service.DeliveryService;
 import com.tookscan.tookscan.order.domain.service.OrderService;
+import com.tookscan.tookscan.order.domain.type.EOrderStatus;
 import com.tookscan.tookscan.order.presentation.dto.request.UpdateAdminOrderDeliveryTrackingNumberRequestDto;
 import com.tookscan.tookscan.order.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,10 @@ public class UpdateAdminOrderDeliveryTrackingNumberService implements UpdateAdmi
     @Transactional
     public void execute(Long deliveryId, UpdateAdminOrderDeliveryTrackingNumberRequestDto requestDto) {
         Delivery delivery = deliveryRepository.findByIdWithOrderOrElseThrow(deliveryId);
+
+        orderService.validateOrderStatus(delivery.getOrder(), EOrderStatus.POST_WAITING,
+                ErrorCode.INVALID_ORDER_STATUS);
+
         deliveryService.updateTrackingNumber(
                 delivery,
                 requestDto.trackingNumber()
