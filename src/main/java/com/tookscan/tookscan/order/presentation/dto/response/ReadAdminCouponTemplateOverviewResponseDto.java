233052c6ffa6ee1936a65ab91a0df539b11dfb4c
GiHwan2 <sandwich5011@naver.com>
2025-08-13 23:30:48 +0900

    Fix/762: 관리자 쿠폰 목록 조회 버그 수정

--- src/main/java/com/tookscan/tookscan/order/presentation/dto/response/ReadAdminCouponTemplateOverviewResponseDto.java
+++ src/main/java/com/tookscan/tookscan/order/presentation/dto/response/ReadAdminCouponTemplateOverviewResponseDto.java
@@ -130,15 +130,7 @@ public class ReadAdminCouponTemplateOverviewResponseDto {
                 description = ECouponType.DELIVERY_PRICE_FREE.getDescription();
             }
 
-            int usedCountSum = couponTemplate.getIssuedCoupons().stream()
-                    .mapToInt(IssuedCoupon::getUsedCount)
-                    .sum();
-
-            Integer maxUsedCountSum = couponTemplate.getMaxUsedPerUserCount() != null
-                    ? couponTemplate.getIssuedCoupons().stream()
-                    .mapToInt(IssuedCoupon::getMaxUsedCount)
-                    .sum()
-                    : null;
+            List<IssuedCoupon> issuedCoupons = couponTemplate.getIssuedCoupons();
 
             return CouponOverviewDto.builder()
                     .id(couponTemplate.getId().toString())
@@ -153,8 +145,13 @@ public class ReadAdminCouponTemplateOverviewResponseDto {
                             : "진행중")
                     .format(couponTemplate.getFormat().getDescription())
                     .type(couponTemplate.getType().getDescription())
-                    .usedCount(usedCountSum)
-                    .maxUsedCount(maxUsedCountSum)
+                    .usedCount(issuedCoupons.stream()
+                            .mapToInt(IssuedCoupon::getUsedCount)
+                            .sum())
+                    .maxUsedCount(issuedCoupons.get(0).getMaxUsedCount() != null
+                            ? issuedCoupons.stream()
+                            .mapToInt(IssuedCoupon::getMaxUsedCount)
+                            .sum() : null)
                     .startAt(couponTemplate.getStartDateTime() != null ? couponTemplate.getStartDateTime().toString() : null)
                     .endAt(couponTemplate.getEndDateTime() != null ? couponTemplate.getEndDateTime().toString() : null)
                     .createdAt(couponTemplate.getCreatedAt().toString())
